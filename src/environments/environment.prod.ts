868b0dfb1c722fad229146d9cd5959e89a674ec3
Sriram Sureshkumar <M1033257@mindtree.com>
2018-05-17 20:20:14 +0530

    Added firebase configuration to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDtBB18DiVoq46VdOoVHKDaCfk_VgwfoTc',
+    authDomain: 'blooddonation.app',
+    databaseURL: 'https://blooddonation-007.firebaseio.com',
+    projectId: 'blooddonation-007',
+    storageBucket: 'blooddonation-007.appspot.com',
+    messagingSenderId: '15717255599'
+  }
 };
