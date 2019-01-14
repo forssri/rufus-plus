274d6cfa45ce47679455fbde07fb15cf67570dc1
Sriram Sureshkumar <sriram.sureshkumar@viacomcontractor.com>
2019-01-14 12:22:33 +0530

    Updated to latest packages

--- src/app/effects/user.effects.ts
+++ src/app/effects/user.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { firebase } from '@firebase/app';
@@ -22,91 +22,100 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class UserEffects {
-    constructor(
-        private actions: Actions,
-        private afAuth: AngularFireAuth,
-        private afs: AngularFirestore,
-        private router: Router
-    ) {
-    }
+  constructor(
+    private actions: Actions,
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {}
 
-    @Effect()
-    getUser: Observable<Action> = this.actions.ofType(userActions.GET_USER)
-        .map((action: userActions.GetUser) => action.payload)
-        .switchMap(payload => this.afAuth.user)
-        .map(authData => {
-            if (authData) {
-                const user = new User(authData.uid, authData.displayName);
-                return new userActions.Authenticated(user);
-            } else {
-                return new userActions.NotAuthenticated();
-            }
-        })
-        .catch(err => of(new userActions.AuthError()));
+  @Effect()
+  getUser: Observable<Action> = this.actions
+    .pipe(ofType(userActions.GET_USER))
+    .map((action: userActions.GetUser) => action.payload)
+    .switchMap(payload => this.afAuth.user)
+    .map(authData => {
+      if (authData) {
+        const user = new User(authData.uid, authData.displayName);
+        return new userActions.Authenticated(user);
+      } else {
+        return new userActions.NotAuthenticated();
+      }
+    })
+    .catch(err => of(new userActions.AuthError()));
 
-    @Effect()
-    login: Observable<Action> = this.actions.ofType(userActions.GOOGLE_LOGIN)
-        .map((action: userActions.GoogleLogin) => action.payload)
-        .switchMap(payload => {
-            return Observable.fromPromise(this.googleLogin());
-        })
-        .map(credential => {
-            return new userActions.GetUser();
-        })
-        .catch(err => of(new userActions.AuthError({ error: err.message })));
+  @Effect()
+  login: Observable<Action> = this.actions
+    .pipe(ofType(userActions.GOOGLE_LOGIN))
+    .map((action: userActions.GoogleLogin) => action.payload)
+    .switchMap(payload => {
+      return Observable.fromPromise(this.googleLogin());
+    })
+    .map(credential => {
+      return new userActions.GetUser();
+    })
+    .catch(err => of(new userActions.AuthError({ error: err.message })));
 
-    @Effect()
-    logout: Observable<Action> = this.actions.ofType(userActions.LOGOUT)
-        .map((action: userActions.Logout) => action.payload)
-        .switchMap(payload => {
-            return of(this.afAuth.auth.signOut());
-        })
-        .map(authData => {
-            return new userActions.NotAuthenticated();
-        })
-        .catch(err => of(new userActions.AuthError({ error: err.message })));
+  @Effect()
+  logout: Observable<Action> = this.actions
+    .pipe(ofType(userActions.LOGOUT))
+    .map((action: userActions.Logout) => action.payload)
+    .switchMap(payload => {
+      return of(this.afAuth.auth.signOut());
+    })
+    .map(authData => {
+      return new userActions.NotAuthenticated();
+    })
+    .catch(err => of(new userActions.AuthError({ error: err.message })));
 
-    @Effect()
-    authenticated: Observable<Action> = this.actions.ofType(userActions.AUTHENTICATED)
-        .map((action: userActions.Authenticated) => action.payload)
-        .switchMap(payload => {
-            const ref = this.afs.doc<Profile>(`users/${payload.uid}`);
-            return ref.valueChanges();
-        })
-        .map(profile => {
-            if (profile) {
-                return new userActions.GetProfile(profile);
-            } else {
-                return new userActions.FirstLogin(profile);
-            }
-        });
-    @Effect()
-    updateProfile: Observable<Action> = this.actions.ofType(userActions.UPDATE_PROFILE)
-        .map((action: userActions.UpdateProfile) => action.payload)
-        .switchMap(payload => {
-            const ref = this.afs.doc<Profile>(`users/${payload.uid}`);
-            return Observable.fromPromise(ref.set(payload.profile));
-        })
-        .map((d) => {
-            console.log(d);
-            return new userActions.UpdateProfileSuccess();
-        });
-    @Effect({ dispatch: false })
-    firstLogin: Observable<Action> = this.actions.ofType(userActions.FIRST_LOGIN)
-        .map((action: userActions.FirstLogin) => action.payload)
-        .switchMap(payload => {
-            if (!payload) {
-                return Observable.fromPromise(this.router.navigate(['profile']));
-            }
-        });
-    @Effect({ dispatch: false })
-    logOut: Observable<Action> = this.actions.ofType(userActions.LOGOUT, userActions.UPDATE_PROFILE_SUCCESS)
-        .map((action: userActions.Logout | userActions.UpdateProfileSuccess) => action.payload)
-        .switchMap(payload => {
-            return Observable.fromPromise(this.router.navigate(['/']));
-        });
-    private googleLogin(): Promise<any> {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        return this.afAuth.auth.signInWithPopup(provider);
-    }
+  @Effect()
+  authenticated: Observable<Action> = this.actions
+    .pipe(ofType(userActions.AUTHENTICATED))
+    .map((action: userActions.Authenticated) => action.payload)
+    .switchMap(payload => {
+      const ref = this.afs.doc<Profile>(`users/${payload.uid}`);
+      return ref.valueChanges();
+    })
+    .map(profile => {
+      if (profile) {
+        return new userActions.GetProfile(profile);
+      } else {
+        return new userActions.FirstLogin(profile);
+      }
+    });
+  @Effect()
+  updateProfile: Observable<Action> = this.actions
+    .pipe(ofType(userActions.UPDATE_PROFILE))
+    .map((action: userActions.UpdateProfile) => action.payload)
+    .switchMap(payload => {
+      const ref = this.afs.doc<Profile>(`users/${payload.uid}`);
+      return Observable.fromPromise(ref.set(payload.profile));
+    })
+    .map(d => {
+      console.log(d);
+      return new userActions.UpdateProfileSuccess();
+    });
+  @Effect({ dispatch: false })
+  firstLogin: Observable<Action> = this.actions
+    .pipe(ofType(userActions.FIRST_LOGIN))
+    .map((action: userActions.FirstLogin) => action.payload)
+    .switchMap(payload => {
+      if (!payload) {
+        return Observable.fromPromise(this.router.navigate(['profile']));
+      }
+    });
+  @Effect({ dispatch: false })
+  logOut: Observable<Action> = this.actions
+    .pipe(ofType(userActions.LOGOUT, userActions.UPDATE_PROFILE_SUCCESS))
+    .map(
+      (action: userActions.Logout | userActions.UpdateProfileSuccess) =>
+        action.payload
+    )
+    .switchMap(payload => {
+      return Observable.fromPromise(this.router.navigate(['/']));
+    });
+  private googleLogin(): Promise<any> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.afAuth.auth.signInWithPopup(provider);
+  }
 }
